3f4c1d985321539b0175cc5ee623b05153013272
vinoth-29 <palanisps2508@gmail.com>
2026-06-27 23:58:19 +0530

    Configure Nitro for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  nitro: process.env.VERCEL ? true : false,
-
   tanstackStart: {
-    server: {
-      entry: "server",
-    },
+    server: { entry: "server" },
+  },
+  nitro: {
+    preset: process.env.VERCEL ? "vercel" : undefined,
   },
 });
